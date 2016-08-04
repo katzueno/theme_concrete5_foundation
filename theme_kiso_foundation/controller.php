7c7699133c5ef90d4bed8fd343353f46beedcfd6
Katz Ueno <katz@concrete5.co.jp>
2016-08-04 18:23:03 +0900

    Basic Package

---
+++ theme_kiso_foundation/controller.php
@@ -0,0 +1,34 @@
+<?php 
+namespace Concrete\Package\ThemeStucco;
+
+use Package;
+use PageTheme;
+
+defined('C5_EXECUTE') or die(_("Access Denied."));
+
+class Controller extends Package
+{
+
+	protected $pkgHandle = 'theme_kiso_foundation';
+	protected $appVersionRequired = '5.7.3';
+	protected $pkgVersion = '0.9.0';
+	protected $pkgAllowsFullContentSwap = true;
+
+	public function getPackageDescription()
+	{
+    	return t("Basic starter theme using Foundation 6.");
+	}
+
+	public function getPackageName()
+	{
+    	return t("Kiso for Foundation");
+	}
+
+	public function install()
+	{
+    	$pkg = parent::install();
+		PageTheme::add('kiso_foundation', $pkg);
+	}
+
+}
+?>
